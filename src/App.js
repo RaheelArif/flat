adf0960b1a2c52d3d35f331a4f09a978e23b293f
RaheelArif <phuawei292@gmail.com>
2019-07-18 14:24:32 +0500

    login form added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Home from './components/home/home';
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import About from './components/about/about';
-import Contact from "./components/contact/contant"
+import Contact from "./components/contact/contant";
+import SignUp from './components/auth/signUp';
+import Login from './components/auth/login';
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -15,6 +18,8 @@ function App() {
          <Route exact path="/" component= {Home} />
          <Route path="/about" component= {About} />
          <Route path="/contact" component= {Contact} />
+         <Route path="/login" component= {Login} />
+         <Route path="/signup" component= {SignUp} />
         </Switch>
         <Footer />
       </div>
