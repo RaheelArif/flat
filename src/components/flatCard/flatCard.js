4fa84a4022640bfd2d76af5b85cee331257d71eb
Muhammad Wajid <muhammad.wajid.shahid@gmail.com>
2019-07-19 11:21:56 +0500

    maded see more funcionnality

--- src/components/flatCard/flatCard.js
+++ src/components/flatCard/flatCard.js
@@ -8,11 +8,13 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import house from "../../images/house.jpg";
+import {Grid } from "@material-ui/core";
 
 
 export default function MediaCard(props) {
 const {property}=props;
   return (
+    <Grid item lg={4} >
     <Card className="flat-card">
       <CardActionArea>
         <CardMedia
@@ -22,10 +24,10 @@ const {property}=props;
         />
         <CardContent className="flat-card-content">
           <Typography gutterBottom variant="h5" component="h2">
-            {property.city}
+            {property.title}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-           {property.title}
+           {property.description}
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -35,5 +37,6 @@ const {property}=props;
          <span>price:{property.price}</span>
       </CardActions>
     </Card>
+    </Grid>
   );
 }
--- src/components/flatCard/threeCardList.js
+++ src/components/flatCard/threeCardList.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import FlatCard from "./flatCard"
 import {connect} from "react-redux"
+import {Grid} from "@material-ui/core"
 class ThreeCardList extends Component {
-
-
+constructor(props){
+    super(props);
+    this.state={
+        view:3
+    }
+}
+changeView=()=>{
+    this.setState({
+        view:this.state.view+6
+    })
+}
     render() {
         return (
             <div className="three-flat-card">
                 <h1>Search Your Dream Home</h1>
-                <div className="row">
-                    
+                {/* <div className="row"> */}
+                    <Grid container lg={10}>
                 {
-                    this.props.properties.length>0&&this.props.properties.map(prop=>{
+                    this.props.properties.length>0&&this.props.properties.map((prop,key)=>{
+                        if(key<this.state.view)
                         return <FlatCard property={prop}/>
                     })
     }
-                </div>
-                <button className="search-btn">View More </button>             
-            </div>
+                </Grid>           
+                {/* </div> */}
+                <button className="search-btn" onClick={this.changeView}>View More </button>  
+             </div>
         )
     }
 }
